8aa78618237fdb16e2b34b88c1f6046e83e9a0e3
MohammadReza Mahmoodi <Saman@DESKTOP-GDO51AR>
2022-05-12 15:34:41 +0430

    add device name library to project

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.learningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaredrummler.android.device.DeviceName
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     var test: ConnectionHandler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        DeviceName.init(this)
         this.test = ConnectionHandler()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
