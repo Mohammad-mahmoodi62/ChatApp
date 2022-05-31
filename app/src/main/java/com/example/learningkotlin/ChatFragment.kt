dfb0647330a86a91f193cdd6dbf4f35e0bd9c081
MohammadReza Mahmoodi <Saman@DESKTOP-GDO51AR>
2022-05-31 19:07:20 +0430

    update chat page UI

--- app/src/main/java/com/example/learningkotlin/ChatFragment.kt
+++ app/src/main/java/com/example/learningkotlin/ChatFragment.kt
@@ -1,12 +1,15 @@
 package com.example.learningkotlin
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
@@ -26,6 +29,8 @@ class ChatFragment : Fragment() {
         val userID = ChatFragmentArgs.fromBundle(requireArguments()).userID
         val currentUser = ConnectionHandler.getUser(userID)
 
+        this.updateToolBar(currentUser!!)
+
         val viewModelFactory = ChatViewModelFactory(currentUser!!.IP)
         val viewModel = ViewModelProvider(
             this, viewModelFactory).get(ChatViewModel::class.java)
@@ -44,8 +49,30 @@ class ChatFragment : Fragment() {
             }
         })
 
-        val sendBtn = view.findViewById<Button>(R.id.send_button)
+        val sendBtn = view.findViewById<ImageView>(R.id.send_button)
         val userMsg = view.findViewById<EditText>(R.id.user_message)
+
+        userMsg.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                return
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if(p0?.length!! > 0) {
+                    val newColor = context?.resources?.getColor(R.color.main_color)
+                    sendBtn.setColorFilter(newColor!!)
+                }
+                else if (p0.isEmpty()) {
+                    val newColor = context?.resources?.getColor(R.color.secondary_color)
+                    sendBtn.setColorFilter(newColor!!)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                return
+            }
+
+        })
 
         sendBtn.setOnClickListener {
             val worker = Runnable {
@@ -61,6 +88,13 @@ class ChatFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun updateToolBar(currentUser: UserInfo) {
+        (activity as MainActivity).toolbarText.text = currentUser.Name
+        //TODO add Image as well
+        (activity as MainActivity).toolbarImage.visibility = View.VISIBLE
+        (activity as MainActivity).supportActionBar?.title = ""
     }
 
 }
